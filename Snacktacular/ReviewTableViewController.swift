cca564731b07bfc736b5a2ac2bcf64ed4d7174d3
Carter Borchetta <carterborch@me.com>
2019-12-02 12:25:39 -0500

    9.22 Complete

--- Snacktacular/ReviewTableViewController.swift
+++ Snacktacular/ReviewTableViewController.swift
@@ -147,6 +147,13 @@ class ReviewTableViewController: UITableViewController {
     
     
     @IBAction func deleteButtonPressed(_ sender: UIButton) {
+        review.deleteData(spot: spot) { (success) in
+            if success {
+                self.leaveViewController()
+            } else {
+                print("Error Delete unsuccesful")
+            }
+        }
     }
     
     
